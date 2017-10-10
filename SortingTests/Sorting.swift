218a038807ef1b6b14a8494266508b58995b605d
Ennio Masi <EnnioMasi@users.noreply.github.com>
2017-10-10 15:38:11 +0100

    Examples added

---
+++ SortingTests/Sorting.swift
@@ -0,0 +1,151 @@
+//
+//  Sorting.swift
+//  SortingTests
+//
+//  Created by Ennio Masi on 26/09/2017.
+//  Copyright © 2017 ennioma. All rights reserved.
+//
+
+func swap<T: Comparable>(left: inout T, right: inout T) {
+    print("Swapping \(left) and \(right)")
+    let temp = right
+    right = left
+    left = temp
+}
+
+import Foundation
+
+extension Array where Element: Comparable {
+    
+    func bubbleSort(by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> [Element] {
+        var data = self
+        
+        for i in 0..<(data.count-1) {
+            for j in 0..<(data.count-i-1) where areInIncreasingOrder(data[j+1], data[j]) {
+                data.swapAt(j, j + 1)
+            }
+        }
+        
+        return data
+    }
+    
+    func insertionSort(by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> [Element] {
+        
+        var data = self
+        
+        for i in 1..<data.count {
+            let key = data[i]
+            var j = i - 1
+            
+            while j >= 0 && areInIncreasingOrder(key, data[j]) {
+                data[j+1] = data[j]
+                
+                j = j - 1
+            }
+            
+            data[j + 1] = key
+        }
+        
+        return data
+    }
+    
+    func selectionSort(by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> [Element] {
+        var data = self
+        
+        for i in 0..<(data.count-1) {
+            var currentMin = i
+            
+            for j in i+1..<data.count where areInIncreasingOrder(data[j], data[currentMin]) {
+                currentMin = j
+            }
+            
+            guard i != currentMin else { continue }
+            
+            data.swapAt(i, currentMin)
+        }
+        
+        return data
+    }
+    
+    private func _quickSort(_ array: [Element], by areInIncreasingOrder: ((Element, Element) -> Bool)) -> [Element] {
+        if array.isEmpty { return array }
+        
+        var data = array
+        
+        let pivot = data.remove(at: 0)
+        let left = data.filter { areInIncreasingOrder($0, pivot) }
+        let right = data.filter { !areInIncreasingOrder($0, pivot) }
+        let middle = [pivot]
+        
+        return _quickSort(left, by: areInIncreasingOrder) + middle + _quickSort(right, by: areInIncreasingOrder)
+    }
+    
+    func quickSort(by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> [Element] {
+        return _quickSort(self, by: areInIncreasingOrder)
+    }
+    
+    private func merge(left: [Element], right: [Element], by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> [Element] {
+        var output: [Element] = []
+        
+        var mutableLeft = left
+        var mutableRight = right
+        while mutableLeft.count > 0 && mutableRight.count > 0 {
+            
+            if let firstElement = mutableLeft.first, let secondElement = mutableRight.first {
+                
+                if !areInIncreasingOrder(firstElement, secondElement) {
+                    output.append(secondElement)
+                    mutableRight.remove(at: 0)
+                } else {
+                    output.append(firstElement)
+                    mutableLeft.remove(at: 0)
+                }
+            }
+        }
+        
+        output.append(contentsOf: mutableLeft)
+        output.append(contentsOf: mutableRight)
+        
+        return output
+    }
+     
+    private func _emMergeSort(data: [Element], by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> [Element] {
+        if data.count < 2 { return data }
+        
+        let leftArray = Array(data[..<Int(data.count / 2)])
+        let rightArray = Array(data[Int(data.count / 2)..<data.count])
+
+        return merge(left: _emMergeSort(data: leftArray, by: areInIncreasingOrder), right: _emMergeSort(data: rightArray, by: areInIncreasingOrder), by: areInIncreasingOrder)
+    }
+    
+    func emMergeSort(by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> [Element] {
+        let data = self
+        
+        return _emMergeSort(data: data, by: areInIncreasingOrder)
+    }
+    
+}
+
+extension Array where Element == Int {
+    func bucketSort(reverse: Bool = false) -> [Element] {
+        var data = self
+        
+        guard data.count > 0 else { return [] }
+        
+        let max = data.max()!
+        var buckets = [Int](repeating: 0, count: (max + 1))
+        var out = [Int]()
+        
+        for i in 0..<data.count {
+            buckets[data[i]] = buckets[data[i]] + 1
+        }
+
+        buckets.enumerated().forEach { index, value in
+            guard value > 0 else { return }
+            
+            out.append(contentsOf: [Int](repeating: index, count: buckets[value]))
+        }
+
+        return reverse == true ? out.reversed() : out
+    }
+}
---
+++ SortingTestsTests/SortingTestsTests.swift
@@ -0,0 +1,122 @@
+//
+//  SortingTestsTests.swift
+//  SortingTestsTests
+//
+//  Created by Ennio Masi on 26/09/2017.
+//  Copyright © 2017 ennioma. All rights reserved.
+//
+
+import XCTest
+@testable import SortingTests
+
+class SortingTestsTests: XCTestCase {
+    
+    var inputArray: [Int] = [100, 1, 2, 0, 50, 23, 22, 21, 10, 101]
+    var ascending: [Int] = [0, 1, 2, 10, 21, 22, 23, 50, 100, 101]
+    var descending: [Int] = [101, 100, 50, 23, 22, 21, 10, 2, 1, 0]
+
+    // MARK: - Bubble Sort
+    func testBubbleSortAscendingWithoutArgs() {
+        
+        XCTAssertEqual(inputArray.bubbleSort(), ascending)
+    }
+    
+    func testBubbleSortAscending() {
+        
+        XCTAssertEqual(inputArray.bubbleSort(by: <), ascending)
+    }
+    
+    func testBubbleSortDescending() {
+        
+        XCTAssertEqual(inputArray.bubbleSort(by: >), descending)
+    }
+    
+    // MARK: - Insertion Sort
+    func testInsertionSortAscendingWithoutArgs() {
+        
+        XCTAssertEqual(inputArray.insertionSort(), ascending)
+    }
+    
+    func testInsertionSortAscending() {
+        
+        XCTAssertEqual(inputArray.insertionSort(by: <), ascending)
+    }
+    
+    func testInsertionSortDescending() {
+        
+        XCTAssertEqual(inputArray.insertionSort(by: >), descending)
+    }
+    
+    // MARK: - Selection Sort
+    func testSelectionSortAscendingWithoutArgs() {
+        
+        XCTAssertEqual(inputArray.selectionSort(), ascending)
+    }
+    
+    func testSelectionSortAscending() {
+        
+        XCTAssertEqual(inputArray.selectionSort(by: <), ascending)
+    }
+    
+    func testSelectionSortDescending() {
+        
+        XCTAssertEqual(inputArray.selectionSort(by: >), descending)
+    }
+
+    // MARK: - Merge Sort
+    func testMergeSortAscendingWithoutArgs() {
+        
+        XCTAssertEqual(inputArray.emMergeSort(), ascending)
+    }
+    
+    func testMergeSortAscending() {
+        
+        XCTAssertEqual(inputArray.emMergeSort(by: <), ascending)
+    }
+    
+    func testMergeSortDescending() {
+        
+        XCTAssertEqual(inputArray.emMergeSort(by: >), descending)
+    }
+    
+    // MARK: - Quick Sort
+    func testQuickSortAscendingWithoutArgs() {
+        
+        XCTAssertEqual(inputArray.quickSort(), ascending)
+    }
+    
+    func testQuickSortAscending() {
+        
+        XCTAssertEqual(inputArray.quickSort(by: <), ascending)
+    }
+    
+    func testQuickSortDescending() {
+        
+        XCTAssertEqual(inputArray.quickSort(by: >), descending)
+    }
+    
+    // MARK: - Bucket Sort
+    func testBucketSortAscendingWithoutArgs() {
+        
+        XCTAssertEqual(inputArray.bucketSort(), ascending)
+    }
+    
+    func testBucketSortAscending() {
+        
+        XCTAssertEqual(inputArray.bucketSort(reverse: false), ascending)
+    }
+    
+    func testBucketSortDescending() {
+        
+        XCTAssertEqual(inputArray.bucketSort(reverse: true), descending)
+    }
+    
+    func testPerformanceExample() {
+        var randomArray = (1...5000).map{ _ in arc4random() }
+        
+        self.measure {
+            let data = randomArray.quickSort()
+        }
+    }
+    
+}
